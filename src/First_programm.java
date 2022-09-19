072160f469913c44d1d16713f9128c100f060510
Mishkagif <asera1995@gmail.com>
2022-09-19 18:04:32 +0300

    New time to learn Java

--- src/First_programm.java
+++ src/First_programm.java
@@ -1,6 +1,12 @@
 public class First_programm {
     public static void main(String[] args) {
         System.out.println("Hello, dear");
+        String s1 = "java";
+        String s2 = "java";
+        String str = new String("java");
+
+        System.out.println(s1.equals(s2));
+        System.out.println(s2.equals(str));
     }
 }
 
