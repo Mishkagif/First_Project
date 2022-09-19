8d25b848fce21deee39283c286b82604d501e867
Mishkagif <asera1995@gmail.com>
2022-09-19 19:14:50 +0300

    New time to learn Java

--- src/String_study.java
+++ src/String_study.java
@@ -1,11 +1,7 @@
-import java.time.LocalDate;
-
 public class String_study {
     public static void main(String[] args) {
+        String s =String.format("Value: %06d",555);
+        System.out.println(s);
 
-        String name = "Mishka";
-        int newMails = 34;
-        String str1 = String.format("Привіт %s, у тебе %d нових повідомлень Сьогодні %s", name, newMails, LocalDate.now());
-        System.out.println(str1);
     }
 }
