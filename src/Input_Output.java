33d91f575acc3aadbcca2fd71ae6ccb20acc57be
Mishkagif <asera1995@gmail.com>
2022-09-20 11:22:24 +0300

    New time to learn Java

--- src/Input_Output.java
+++ src/Input_Output.java
@@ -1,14 +1,16 @@
-import java.util.Scanner;
-
 public class Input_Output {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+      /**
+       * Scanner код для зручного використання <>b</>
+       * @autor Mishka
+       * @since 1.0
+       * Scanner scan = new Scanner(System.in);
         System.out.print("Enter your name: ");
         String name = scan.nextLine();
         System.out.print("Enter your age:");
         int age = scan.nextInt();
 
-        System.out.printf("Name: %s, Age: %d ", name, age);
+        System.out.printf("Name: %s, Age: %d ", name, age);*/
 
 
 
