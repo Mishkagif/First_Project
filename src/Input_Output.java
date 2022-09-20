641fe4a3fb82210f5625343e12ecfa1edab837f2
Mishkagif <asera1995@gmail.com>
2022-09-20 10:57:45 +0300

    New time to learn Java

---
+++ src/Input_Output.java
@@ -0,0 +1,11 @@
+public class Input_Output {
+    public static void main(String[] args) {
+        System.out.println("Hello, world\n");
+        System.out.println("Hello, world\t");
+
+
+    }
+
+
+
+}
