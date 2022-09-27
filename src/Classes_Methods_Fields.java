b8dda622cb6a67ff1bcc14e597b5fb740bd4ea69
Mishkagif <asera1995@gmail.com>
2022-09-27 19:49:03 +0300

    The new lesson OOP and update my knowledge

---
+++ src/Classes_Methods_Fields.java
@@ -0,0 +1,11 @@
+public class Classes_Methods_Fields {
+    public static void main(String[] args) {
+        Professor professorAlex = new Professor("Alex","Mikuleso",10);
+        String mail=professorAlex.mail();
+        System.out.println(mail);
+
+    }
+
+
+}
+
---
+++ src/Professor.java
@@ -0,0 +1,27 @@
+public class Professor {
+    public static void main(String[] args) {
+        String name;
+
+    }
+     String name;
+    String surname;
+   int experience;
+
+
+    Professor(String name, String surname, int experience) {
+        this.name = name;
+        this.surname = surname;
+        this.experience = experience;
+    }
+
+
+
+    String mail(String message) {
+
+    return name + ": Send message";
+    }
+}
+
+
+
+
