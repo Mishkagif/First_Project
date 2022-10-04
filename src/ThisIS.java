4a1e82f1ead33e901a63aea20aa5aaa920b23f3d
Mishkagif <asera1995@gmail.com>
2022-10-04 09:06:16 +0300

    Changed all Class

---
+++ src/ThisIS.java
@@ -0,0 +1,9 @@
+public class This_is {
+    public static void main(String[] args) {
+        User user = new User("John", "Travolta");
+        user.print();
+        System.out.println(user.name);
+
+    }
+}
+
